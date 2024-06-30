df4b85172427f4fceaac4c032210db7cb643a3f0
Desmond Awere <desmondawere@gmail.com>
2024-06-30 14:44:52 +0100

    first commit

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_TEST_SECRET_KEY!, {
-  // apiVersion: "2023-08-16",
+  apiVersion: "2024-06-20",
   typescript: true,
 });
